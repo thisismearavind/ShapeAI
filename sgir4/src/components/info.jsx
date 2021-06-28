22ce0758f45d46272450fb74af255e02d64b8dc2
thisismearavind <76204589+thisismearavind@users.noreply.github.com>
2021-06-29 00:33:48 +0530

    Add files via upload

---
+++ sgir4/src/components/info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function info() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js </h1>
+      <p>A basic web React Js Bootcamp</p>
+    </div>
+  );
+}
+
+export default info;
